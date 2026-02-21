665af58fc4647222392c9aa6a2596309301d48ad
Harsh21-design <51110904133@piemr.edu.in>
2026-02-21 23:09:34 +0530

    update patient.json & main.py

--- Patients Management/main.py
+++ Patients Management/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 app = FastAPI()
 
@@ -51,3 +51,33 @@ def get_patient(patients_id: str = Path(...,description="ID of the Patient in th
     # HTTP Error Response ->status code
     raise HTTPException(status_code=404,detail=f"Patient with ID '{patients_id}' is NOT FOUND!")
 
+# get sorted patients : Example of Query Params
+@app.get("/sort")
+def get_sorted_patients(sort_by: str = Query(..., description="Sort on the basis of height,weight or age"),order_by: str = Query('asc', description="Sort in asc or desc order" )):
+    # valid sorting field in url
+    valid_sorted_fields = ['weight','height','age']
+
+    # validation rule/error handling
+    if sort_by not in valid_sorted_fields:
+        raise HTTPException(status_code=400,detail=f"Invalid fields select from {valid_sorted_fields}") # 400-> Bad Request
+    
+    if order_by not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail=f"Invalid order select between asc and desc")
+    
+    # get all patient
+    patient_data = get_all_patients()
+
+    # sort order reverse rule
+    sort_order = True if order_by == 'desc' else False
+
+    # sorting logic by sorted() func
+    sorted_patient_data = sorted(
+        patient_data.values(), # only dict values
+        key=lambda x: x.get(sort_by,0), # dict key based rule
+        reverse=sort_order # true/false
+    )
+
+    return sorted_patient_data
+    
+    # example endpoint for testing
+    # http://127.0.0.1:8000/sort?sort_by=height&order_by=desc
