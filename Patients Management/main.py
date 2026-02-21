b64fd55fbb9a30f37d35d0e98b4d5a04ef9a2182
Harsh21-design <51110904133@piemr.edu.in>
2026-02-21 16:00:16 +0530

    initialize and setup files

---
+++ Patients Management/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Path, HTTPException
+import json
+app = FastAPI()
+
+# Load Patient Data
+def load_patients_data():
+    with open("patients.json","r") as file:
+        patients_data = json.load(file)
+
+    return patients_data
+
+@app.get("/")
+def home():
+    return {
+        "message":"Welcome to our FastAPI App - Patient Management APIs System For Doctors"
+    }
+
+@app.get("/about")
+def about():
+    return {
+        "message":"A fully funtional APIs to Manage Patient's Record."
+    }
+
+@app.get("/patients")
+def get_patients():
+    all_patients = load_patients_data()
+    return all_patients
+
+@app.get("/patients/{patients_id}")
+def get_patient(patients_id: str = Path(...,description="ID of the Patient in the Database",example="P001")):
+    patients = load_patients_data()
+    if patients_id in patients:
+        return patients[patients_id]
+    
+    # static json response
+    # return {
+    #     "Error":f"Patient with ID '{patients_id}' is NOT FOUND!"
+    # }
+    
+    # HTTP Error Response ->status code
+    raise HTTPException(status_code=404,detail="Patient Not Found!")
+
---
+++ basic_app.py
@@ -0,0 +1,17 @@
+# # BASIC FASTAPI EXAMPLE
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message":"Hello World!From FastAPI"
+#     }
+
+# @app.get("/about")
+# def about():
+#     return {
+#         "message":"This is a about page of example FastAPI App."
+#     }
+
