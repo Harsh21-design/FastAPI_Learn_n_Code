b6f0c5384ed32172e0fc0d65f311b1037dd73cef
Harsh21-design <51110904133@piemr.edu.in>
2026-02-21 17:51:24 +0530

    changes in main file

--- Patients Management/main.py
+++ Patients Management/main.py
@@ -1,42 +1,53 @@
-# from fastapi import FastAPI, Path, HTTPException
-# import json
-# app = FastAPI()
+from fastapi import FastAPI, Path, HTTPException
+import json
+app = FastAPI()
 
-# # Load Patient Data
-# def load_patients_data():
-#     with open("patients.json","r") as file:
-#         patients_data = json.load(file)
+# Load Patient Data
+def load_patients_data():
+    with open("patients.json","r") as file:
+        patients_data = json.load(file)
 
-#     return patients_data
+    return patients_data
 
-# @app.get("/")
-# def home():
-#     return {
-#         "message":"Welcome to our FastAPI App - Patient Management APIs System For Doctors"
-#     }
+# home page
+@app.get("/")
+def home():
+    return {
+        "message":"Welcome to our FastAPI App - Patient Management APIs System For Doctors"
+    }
 
-# @app.get("/about")
-# def about():
-#     return {
-#         "message":"A fully funtional APIs to Manage Patient's Record."
-#     }
+# about page
+@app.get("/about")
+def about():
+    return {
+        "message":"A fully funtional APIs to Manage Patient's Record."
+    }
 
-# @app.get("/patients")
-# def get_patients():
-#     all_patients = load_patients_data()
-#     return all_patients
+# Get all patients
+@app.get("/patients")
+def get_all_patients():
+    all_patients = load_patients_data()
+    return all_patients
 
+# get single patients
 # @app.get("/patients/{patients_id}")
-# def get_patient(patients_id: str = Path(...,description="ID of the Patient in the Database",example="P001")):
-#     patients = load_patients_data()
+# def get_single_patient(patients_id: str):
+#     patients = get_all_patients()
 #     if patients_id in patients:
 #         return patients[patients_id]
     
 #     # static json response
-#     # return {
-#     #     "Error":f"Patient with ID '{patients_id}' is NOT FOUND!"
-#     # }
+#     return {
+#         "Error":f"Patient with ID '{patients_id}' is NOT FOUND!"
+#     }
+
+# get single patients : proper PATH validation & Custom HTTP Exception
+@app.get("/patients/{patients_id}")
+def get_patient(patients_id: str = Path(...,description="ID of the Patient in the Database",example="P001")):
+    patients = load_patients_data()
+    if patients_id in patients:
+        return patients[patients_id]
     
-#     # HTTP Error Response ->status code
-#     raise HTTPException(status_code=404,detail="Patient Not Found!")
+    # HTTP Error Response ->status code
+    raise HTTPException(status_code=404,detail=f"Patient with ID '{patients_id}' is NOT FOUND!")
 
--- basic_app.py
+++ basic_app.py
@@ -1,19 +1,19 @@
 # BASIC FASTAPI EXAMPLE
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def home():
-    return {
-        "message":"Hello World!From FastAPI"
-    }
+# @app.get("/")
+# def home():
+#     return {
+#         "message":"Hello World!From FastAPI"
+#     }
 
-@app.get("/about")
-def about():
-    return {
-        "message":"This is a about page of example FastAPI App."
-    }
+# @app.get("/about")
+# def about():
+#     return {
+#         "message":"This is a about page of example FastAPI App."
+#     }
 
 # Command: Run the code
 # uvicorn main:app.py
